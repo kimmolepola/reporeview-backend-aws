5e62a04fd8c30111eb06236e633260410d57bbf6
Ismo Lepola <kimmo.lepola@gmail.com>
2021-01-20 18:14:36 +0200

    initial

---
+++ index.js
@@ -0,0 +1,6 @@
+// import app from './src/main'
+
+exports.handler =  async function(event, context) {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    return context.logStreamName;
+};
